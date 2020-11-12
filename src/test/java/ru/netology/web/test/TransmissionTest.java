c92fd70fc036785b1d293455cf813a90b0bf128c
Yulia Kholtobina <another.z@rambler.ru>
2020-11-12 15:16:05 +0200

    Test was changed

--- src/test/java/ru/netology/web/test/TransmissionTest.java
+++ src/test/java/ru/netology/web/test/TransmissionTest.java
@@ -15,17 +15,17 @@ public class TransmissionTest {
 
     @BeforeEach
     void setUp () {
-       val loginPage = open("http://localhost:9999", LoginPage.class);
-       val verificationPage = loginPage.validLogin(DataHelper.getAuthInfo());
-       val verificationCode = DataHelper.getVerificationCodeFor();
-       verificationPage.validVerify(verificationCode);
+        val loginPage = open("http://localhost:9999", LoginPage.class);
+        val verificationPage = loginPage.validLogin(DataHelper.getAuthInfo());
+        val verificationCode = DataHelper.getVerificationCodeFor();
+        verificationPage.validVerify(verificationCode);
     }
 
     @Test
     @Order(1)
     void shouldTransmitFromFirstToSecond() {
         val dashboardPage = new DashboardPage();
-        val amount = 1000;
+        String amount = "1000";
         val currentBalanceOfFirstCard = dashboardPage.getCurrentBalanceOfFirstCard();
         val currentBalanceOfSecondCard = dashboardPage.getCurrentBalanceOfSecondCard();
         val transmissionPage = dashboardPage.transmissionToSecondCard();
@@ -33,17 +33,17 @@ public class TransmissionTest {
 
         val balanceOfFirstCardAfterTransmit = dashboardPage.getCurrentBalanceOfFirstCard();
         val balanceOfSecondCardAfterTransmit = dashboardPage.getCurrentBalanceOfSecondCard();
-        val expectedBalanceOfFirstCardAfterTransmit = getExpectedBalanceIfBalanceDecrease(currentBalanceOfFirstCard, amount);
-        val expectedBalanceOfSecondCardAfterTransmit = getExpectedBalanceIfBalanceRise(currentBalanceOfSecondCard, amount);
-        assertEquals(expectedBalanceOfFirstCardAfterTransmit, balanceOfFirstCardAfterTransmit);
-        assertEquals(expectedBalanceOfSecondCardAfterTransmit, balanceOfSecondCardAfterTransmit);
+        val expectedBalanceOfFirstCardAfterTransmit = getExpectedBalanceIfBalanceDecrease(Integer.parseInt(currentBalanceOfFirstCard), Integer.parseInt(amount));
+        val expectedBalanceOfSecondCardAfterTransmit = getExpectedBalanceIfBalanceRise(Integer.parseInt(currentBalanceOfSecondCard), Integer.parseInt(amount));
+        assertEquals(expectedBalanceOfFirstCardAfterTransmit, Integer.parseInt(balanceOfFirstCardAfterTransmit));
+        assertEquals(expectedBalanceOfSecondCardAfterTransmit, Integer.parseInt(balanceOfSecondCardAfterTransmit));
     }
 
     @Test
     @Order(2)
     void shouldTransmitFromSecondToFirst() {
         val dashboardPage = new DashboardPage();
-        val amount = 500;
+        String amount = "500";
         val currentBalanceOfFirstCard = dashboardPage.getCurrentBalanceOfFirstCard();
         val currentBalanceOfSecondCard = dashboardPage.getCurrentBalanceOfSecondCard();
         val transmissionPage = dashboardPage.transmissionToFirstCard();
@@ -51,17 +51,17 @@ public class TransmissionTest {
 
         val balanceOfFirstCardAfterTransmit = dashboardPage.getCurrentBalanceOfFirstCard();
         val balanceOfSecondCardAfterTransmit = dashboardPage.getCurrentBalanceOfSecondCard();
-        val expectedBalanceOfFirstCardAfterTransmit = getExpectedBalanceIfBalanceRise(currentBalanceOfFirstCard, amount);
-        val expectedBalanceOfSecondCardAfterTransmit = getExpectedBalanceIfBalanceDecrease(currentBalanceOfSecondCard, amount);
-        assertEquals(expectedBalanceOfFirstCardAfterTransmit, balanceOfFirstCardAfterTransmit);
-        assertEquals(expectedBalanceOfSecondCardAfterTransmit, balanceOfSecondCardAfterTransmit);
+        val expectedBalanceOfFirstCardAfterTransmit = getExpectedBalanceIfBalanceRise(Integer.parseInt(currentBalanceOfFirstCard), Integer.parseInt(amount));
+        val expectedBalanceOfSecondCardAfterTransmit = getExpectedBalanceIfBalanceDecrease(Integer.parseInt(currentBalanceOfSecondCard), Integer.parseInt(amount));
+        assertEquals(String.valueOf(expectedBalanceOfFirstCardAfterTransmit), balanceOfFirstCardAfterTransmit);
+        assertEquals(String.valueOf(expectedBalanceOfSecondCardAfterTransmit), balanceOfSecondCardAfterTransmit);
     }
 
     @Test
     @Order(3)
     void shouldTransmitZeroAmount() {
         val dashboardPage = new DashboardPage();
-        val amount = 0;
+        String amount = "0";
         val currentBalanceOfFirstCard = dashboardPage.getCurrentBalanceOfFirstCard();
         val currentBalanceOfSecondCard = dashboardPage.getCurrentBalanceOfSecondCard();
         val transmissionPage = dashboardPage.transmissionToSecondCard();
@@ -69,39 +69,40 @@ public class TransmissionTest {
 
         val balanceOfFirstCardAfterTransmit = dashboardPage.getCurrentBalanceOfFirstCard();
         val balanceOfSecondCardAfterTransmit = dashboardPage.getCurrentBalanceOfSecondCard();
-        val expectedBalanceOfFirstCardAfterTransmit = getExpectedBalanceIfBalanceDecrease(currentBalanceOfFirstCard, amount);
-        val expectedBalanceOfSecondCardAfterTransmit = getExpectedBalanceIfBalanceRise(currentBalanceOfSecondCard, amount);
-        assertEquals(expectedBalanceOfFirstCardAfterTransmit, balanceOfFirstCardAfterTransmit);
-        assertEquals(expectedBalanceOfSecondCardAfterTransmit, balanceOfSecondCardAfterTransmit);
+        val expectedBalanceOfFirstCardAfterTransmit = getExpectedBalanceIfBalanceDecrease(Integer.parseInt(currentBalanceOfFirstCard), Integer.parseInt(amount));
+        val expectedBalanceOfSecondCardAfterTransmit = getExpectedBalanceIfBalanceRise(Integer.parseInt(currentBalanceOfSecondCard), Integer.parseInt(amount));
+        assertEquals(String.valueOf(expectedBalanceOfFirstCardAfterTransmit), balanceOfFirstCardAfterTransmit);
+        assertEquals(String.valueOf(expectedBalanceOfSecondCardAfterTransmit), balanceOfSecondCardAfterTransmit);
     }
+
     @Test
     @Order(4)
     void shouldBeErrorIfAmountEmpty() {
         val dashboardPage = new DashboardPage();
         String amount = "";
         val transmissionPage = dashboardPage.transmissionToSecondCard();
-        transmissionPage.transmission(getFirstCardNumber(), Integer.parseInt(amount));
-        transmissionPage.errorTransmittion();
+        transmissionPage.transmission(getFirstCardNumber(), amount);
+        transmissionPage.errorTransmission();
     }
 
     @Test
     @Order(5)
     void shouldBeErrorIfFromInputCardIsEmpty() {
         val dashboardPage = new DashboardPage();
-        val amount = 1000;
+        String amount = "1000";
         val transmissionPage = dashboardPage.transmissionToSecondCard();
         transmissionPage.transmission(getEmptyCardNumber(), amount);
-        transmissionPage.errorTransmittion();
+        transmissionPage.errorTransmission();
     }
 
     @Test
     @Order(6)
     void shouldBeErrorIfNotCorrectCardNumber() {
         val dashboardPage = new DashboardPage();
-        val amount = 1000;
+        String amount = "1000";
         val transmissionPage = dashboardPage.transmissionToSecondCard();
         transmissionPage.transmission(getNotCorrectCardNumber(), amount);
-        transmissionPage.errorTransmittion();
+        transmissionPage.errorTransmission();
     }
 
     @Test
@@ -116,20 +117,20 @@ public class TransmissionTest {
 
         val balanceOfFirstCardAfterTransmit = dashboardPage.getCurrentBalanceOfFirstCard();
         val balanceOfSecondCardAfterTransmit = dashboardPage.getCurrentBalanceOfSecondCard();
-        val expectedBalanceOfFirstCardAfterTransmit = getExpectedBalanceIfBalanceDecrease(currentBalanceOfFirstCard, amount);
-        val expectedBalanceOfSecondCardAfterTransmit = getExpectedBalanceIfBalanceRise(currentBalanceOfSecondCard, amount);
-        assertEquals(expectedBalanceOfFirstCardAfterTransmit, balanceOfFirstCardAfterTransmit);
-        assertEquals(expectedBalanceOfSecondCardAfterTransmit, balanceOfSecondCardAfterTransmit);
+        val expectedBalanceOfFirstCardAfterTransmit = getExpectedBalanceIfBalanceDecrease(Integer.parseInt(currentBalanceOfFirstCard), Integer.parseInt(amount));
+        val expectedBalanceOfSecondCardAfterTransmit = getExpectedBalanceIfBalanceRise(Integer.parseInt(currentBalanceOfSecondCard), Integer.parseInt(amount));
+        assertEquals(String.valueOf(expectedBalanceOfFirstCardAfterTransmit), balanceOfFirstCardAfterTransmit);
+        assertEquals(String.valueOf(expectedBalanceOfSecondCardAfterTransmit), balanceOfSecondCardAfterTransmit);
     }
 
     @Test
     @Order(8)
     void shouldBeErrorIfAmountMoreThanCurrentBalance() {
         val dashboardPage = new DashboardPage();
-        val amount = dashboardPage.getCurrentBalanceOfSecondCard() * 2;
+        val amount = dashboardPage.getCurrentBalanceOfSecondCard() + "0";
         val transmissionPage = dashboardPage.transmissionToFirstCard();
         transmissionPage.transmission(getSecondCardNumber(), amount);
-        transmissionPage.errorTransmittion();
+        transmissionPage.errorTransmission();
     }
 
 }
